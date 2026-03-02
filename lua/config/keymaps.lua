31832f544d016ecb09eff372a747c238a333199a
victoraury <victoraury@gmail.com>
2026-03-02 17:15:01 -0300

    write to register when cut in visual mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,7 +3,7 @@
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>h", Snacks.dashboard.open, { desc = "Open mini starter" })
 vim.keymap.set({ "n", "v" }, "d", '"_d')
-vim.keymap.set({ "n", "v" }, "x", '"_x')
+vim.keymap.set("n", "x", '"_x')
 vim.keymap.set("n", "<leader>Ab", function()
   vim.cmd("DroidRun")
 end, { desc = "Deploy android app" })
